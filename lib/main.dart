f397fcdc7089ea45ef39004036d584241718ff25
naseeb <naseebnaushad46@gmail.com>
2025-08-12 19:58:15 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profile App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.brown[100],
+        appBar: AppBar(
+          backgroundColor: Colors.brown,
+          title: Padding(
+            padding: const EdgeInsets.only(bottom: 15),
+            child: Text(
+              'My Profile',
+              style: GoogleFonts.playfair(
+                fontWeight: FontWeight.w600,
+                fontSize: 35,
+              ),
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage("assets/profile/profile.jpeg"),
+                  foregroundImage: AssetImage('assets/profile/pro.png'),
+                  child: Text('data', style: TextStyle(fontSize: 12)),
+                ),
+              ),
+              SizedBox(height: 30),
+              Text(
+                'Naseeb Naushad',
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Developer \n'
+                'Flutter Enthusiast \n'
+                'Open Source Contributor',
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
