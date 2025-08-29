93bc954c4eb46f0b17a0e285205377d7fb4bfc1d
naseeb <naseebnaushad46@gmail.com>
2025-08-29 22:43:46 +0530

    minor profile modifications

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Pro',
+      title: 'My Profile',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.brown[100],
